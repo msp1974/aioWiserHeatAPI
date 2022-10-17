8b374619257d83cd3917e1aaecd8b5bec2f5f1c3
Mark Parker <msparker@sky.com>
2022-10-17 13:18:38 +0100

    v0.1.0

--- aioWiserHeatAPI/__init__.py
+++ aioWiserHeatAPI/__init__.py
@@ -1,7 +1,8 @@
-name = "wiser-heat-api-v2"
-__all__ = ["wiserAPI","wiserDiscovery"]
+name = "aioWiserHeatAPI"
+__all__ = ["wiserAPI", "wiserDiscovery"]
 
-__VERSION__ = "0.0.1"
+__VERSION__ = "0.1.0"
 
 import logging
+
 _LOGGER = logging.getLogger(__name__)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aioWiserHeatAPI",
-    version="0.0.1",
+    version="0.1.0",
     author="Mark Parker",
     author_email="msparker@sky.com",
     description="An AsyncIO API for controlling the Drayton Wiser Heating system",
