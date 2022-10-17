38a5640bffbf683a0db9aaa874d11a39302ef236
Mark Parker <msparker@sky.com>
2022-10-17 13:32:11 +0100

    amend setup.py to get version from init

--- aioWiserHeatAPI/__init__.py
+++ aioWiserHeatAPI/__init__.py
@@ -1,7 +1,7 @@
 name = "aioWiserHeatAPI"
 __all__ = ["wiserAPI", "wiserDiscovery"]
 
-__VERSION__ = "0.1.0"
+__VERSION__ = "0.1.1"
 
 import logging
 
--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 import setuptools
+import codecs
+import os.path
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__VERSION__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name="aioWiserHeatAPI",
-    version="0.1.0",
+    version=get_version("aioWiserHeatAPI/__init__.py"),
     author="Mark Parker",
     author_email="msparker@sky.com",
     description="An AsyncIO API for controlling the Drayton Wiser Heating system",
