c2bd9184d36575ee249a8a9c6b9f953a1ca2d0b4
Mark Parker <msparker@sky.com>
2022-10-21 14:49:19 +0100

    add aiohttp to requirements

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["zeroconf>=0.39", "aiofiles>=3.8", "pyyaml>=6.0"],
+    install_requires=["zeroconf>=0.39", "aiohttp>=3.8.1", "aiofiles>=3.8", "pyyaml>=6.0"],
     python_requires=">=3.10",
     entry_points={
         "console_scripts": ["wiser = aioWiserHeatAPI.cli:main"],
