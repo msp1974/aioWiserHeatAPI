ca7ae47f251aa319cb9f011ab0f76006e6b35362
Mark Parker <msparker@sky.com>
2023-01-04 18:12:08 +0000

    Add schedule exceptions

--- aioWiserHeatAPI/exceptions.py
+++ aioWiserHeatAPI/exceptions.py
@@ -11,6 +11,18 @@ class WiserHubRESTError(Exception):
     pass
 
 
+class WiserScheduleError(Exception):
+    pass
+
+
+class WiserScheduleInvalidTime(Exception):
+    pass
+
+
+class WiserScheduleInvalidSetting(Exception):
+    pass
+
+
 class WiserHubNotImplementedError(Exception):
     # _LOGGER.info("Function not yet implemented")
     pass
