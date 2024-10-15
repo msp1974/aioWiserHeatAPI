5534588a3026ceaef2b7d1bf05258005554e3d99
Mark Parker <msparker@sky.com>
2024-10-15 18:33:52 +0100

    fix version compare

--- aioWiserHeatAPI/helpers/version.py
+++ aioWiserHeatAPI/helpers/version.py
@@ -11,11 +11,7 @@ class Version:
 
     def __gt__(self, other):
         if isinstance(other, Version):
-            return (
-                self.major >= other.major
-                and self.minor >= other.minor
-                and self.micro > other.micro
-            )
+            return self._version_compare(self.version, other.version) == 1
 
     def __ge__(self, other):
         if isinstance(other, Version):
@@ -23,11 +19,7 @@ class Version:
 
     def __lt__(self, other):
         if isinstance(other, Version):
-            return (
-                self.major <= other.major
-                and self.minor <= other.minor
-                and self.micro < other.micro
-            )
+            return self._version_compare(self.version, other.version) == -1
 
     def __le__(self, other):
         if isinstance(other, Version):
@@ -35,33 +27,22 @@ class Version:
 
     def __eq__(self, other):
         if isinstance(other, Version):
-            return (
-                self.major == other.major
-                and self.minor == other.minor
-                and self.micro == other.micro
-            )
+            return self._version_compare(self.version, other.version) == 0
 
-    @property
-    def major(self) -> int:
-        """Get major version."""
-        try:
-            return self.version.split(".")[0]
-        except IndexError:
-            return None
+    def _version_compare(self, v1: str, v2: str) -> int:
+        version1 = v1.replace("-", ".").split(".")
+        version2 = v2.replace("-", ".").split(".")
 
-    @property
-    def minor(self) -> int:
-        """Get minor version."""
-        try:
-            return self.version.split(".")[1]
-        except IndexError:
-            return None
+        length = max(len(version1), len(version2))
 
-    @property
-    def micro(self) -> int:
-        """Get micro version."""
-        try:
-            full_micro = self.version.split(".")[2]
-            return full_micro.split("-")[0]
-        except IndexError:
-            return None
+        # Compare each component of the version strings.
+        for i in range(length):
+            v1_itm = int(version1[i]) if version1[i].isdigit() else version1[i]
+            v2_itm = int(version2[i]) if version2[i].isdigit() else version2[i]
+
+            if v1_itm > v2_itm:
+                return 1
+            if v1_itm < v2_itm:
+                return -1
+
+        return 0
