f10650f7f3319533159919fb983119cd33f8297a
Mark Parker <msparker@sky.com>
2024-12-03 14:21:31 +0000

    fix: add equipment data on device build

--- aioWiserHeatAPI/devices.py
+++ aioWiserHeatAPI/devices.py
@@ -260,6 +260,12 @@ class _WiserDeviceCollection:
                     else:
                         device_schedule = None
 
+                    # If has equipment data add to device info
+                    if equipment_id := device_type_data.get("EquipmentId"):
+                        device_info_data["EquipmentData"] = self._get_equipment_data(
+                            equipment_id
+                        )
+
                     # Add device to collection
                     self._device_collection[device_type]._items.append(
                         device_class(
